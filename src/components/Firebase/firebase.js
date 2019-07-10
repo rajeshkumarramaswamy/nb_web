c2746d5cb4f95a59fc906089efb4b1ff208ce1e3
rajeshkumarramaswamy <rajeshkramaswamy@gmail.com>
2019-07-10 15:46:01 +0530

    Removed key

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,15 +2,8 @@ import firebase from 'firebase';
 import 'firebase/firestore';
 import 'firebase/auth';
  // Your web app's Firebase configuration
- var firebaseConfig = {
-    apiKey: "AIzaSyDT2lkYRcYFd6ZruHgIftAikR8Wg_NfLHM",
-    authDomain: "nesmatalbustan-ac52c.firebaseapp.com",
-    databaseURL: "https://nesmatalbustan-ac52c.firebaseio.com",
-    projectId: "nesmatalbustan-ac52c",
-    storageBucket: "",
-    messagingSenderId: "832281960880",
-    appId: "1:832281960880:web:35af3ffb6ba333e8"
-  };
+ console.log('FIREBASE', env.REACT_APP_API_KEY)
+
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.firestore().settings({ timestampsInSnapshots: true });
